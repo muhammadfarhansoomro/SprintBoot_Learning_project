e3c6fcd40e3ddb203da161e9b850b9c0dc9e9315
Farhan Soomro <muhammad.soomro@ampada.com>
2021-11-10 10:10:40 +0100

    Committing

--- src/test/java/com/example/selenium/helpers/ConstantsHelper.java
+++ src/test/java/com/example/selenium/helpers/ConstantsHelper.java
@@ -1,6 +1,6 @@
 package com.example.selenium.helpers;
 
 public class ConstantsHelper {
-    public static final String searchInput = "username";
+    public static final String searchInput = "search_form_input_homepage";
     public static final String searchButton = "search_button_homepage";
 }
